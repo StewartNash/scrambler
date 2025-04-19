12e680825f793784706ad29d80650dd666b12aef
StewartNash <Stewart.M.Nash@gmail.com>
2025-04-19 11:42:15 -0700

    Added array subscription.

--- arduino/main.ino
+++ arduino/main.ino
@@ -11,17 +11,27 @@
 #include "scrambler.hpp"
 
 #include <ros.h>
-#include <std_msgs/Empty.h>
+//#include <std_msgs/Empty.h>
 #include <std_msgs/String.h>
+#include <std_msgs/Int16MultiArray.h>
 //#include <Servo.h>
 
 ros::NodeHandle nh;
+Organism myOrganism;
 
+/*
 void messageCallback(const std_msgs::Empty& toggle_message) {
 	digitalWrite(13, HIGH - digitalRead(13)); // Blink the LED
 }
 
 ros::Subscriber<std_msgs::Empty> subscription_("toggle_led", &messageCallback);
+*/
+
+void messageCallback(const std_msgs::Int16MultiArray toggle_message) {
+	digitalWrite(13, HIGH - digitalRead(13)); // Blink the LED
+}
+
+ros::Subscriber<std_msgs::Int16MultiArray> subscription_("toggle_led", &messageCallback);
 
 void setup() {
 	pinMode(13, OUTPUT);
@@ -32,4 +42,43 @@ void setup() {
 void loop() {
 	nh.spinOnce();
 	delay(1);
-} 
+}
+
+void initialize() {
+  myOrganism.addAppendage(Limb(
+  	RIGHT_FRONT_COXA,
+  	RIGHT_FRONT_FEMUR,
+  	RIGHT_FRONT_TIBIA,
+  	0,
+  	"right front leg"));
+  myOrganism.addAppendage(Limb(
+  	RIGHT_MIDDLE_COXA,
+  	RIGHT_MIDDLE_FEMUR,
+  	RIGHT_MIDDLE_TIBIA,
+  	1,
+  	"right middle leg"));
+   myOrganism.addAppendage(Limb(
+  	RIGHT_BACK_COXA,
+  	RIGHT_BACK_FEMUR,
+  	RIGHT_BACK_TIBIA,
+  	2,
+  	"right back leg"));
+  myOrganism.addAppendage(Limb(
+  	LEFT_FRONT_COXA,
+  	LEFT_FRONT_FEMUR,
+  	LEFT_FRONT_TIBIA,
+  	3,
+  	"left front leg"));
+  myOrganism.addAppendage(Limb(
+  	LEFT_MIDDLE_COXA,
+  	LEFT_MIDDLE_FEMUR,
+  	LEFT_MIDDLE_TIBIA,
+  	4,
+  	"left middle leg"));
+   myOrganism.addAppendage(Limb(
+  	LEFT_BACK_COXA,
+  	LEFT_BACK_FEMUR,
+  	LEFT_BACK_TIBIA,
+  	5,
+  	"left back leg"));
+}
--- arduino/scrambler.hpp
+++ arduino/scrambler.hpp
@@ -7,22 +7,7 @@
  *
  */
 #include <Servo.h>
-
-class Limb {
-	public:
-		Servo coxa;
-		Servo femur;
-		Servo tibia;
-		
-		std::string name;
-		int number;
-}
-
-class Organism {
-	public:
-		std::vector<Limb> appendages
-}
-
+#include <vector>
 /*
  * "right front leg": coxa(39), femur(38), tibia(37)
  * "right middle leg": coxa(36), femur(35), tibia(34)
@@ -31,4 +16,71 @@ class Organism {
  * "left middle leg": coxa(25), femur(26), tibia(27)
  * "left back leg": coxa(28), femur(29), tibia(30)
  */
+const int RIGHT_FRONT_COXA = 39;
+const int RIGHT_FRONT_FEMUR = 38;
+const int RIGHT_FRONT_TIBIA = 37;
+const int RIGHT_MIDDLE_COXA = 36;
+const int RIGHT_MIDDLE_FEMUR = 35;
+const int RIGHT_MIDDLE_TIBIA = 34;
+const int RIGHT_BACK_COXA = 33;
+const int RIGHT_BACK_FEMUR = 32;
+const int RIGHT_BACK_TIBIA = 31;
+const int LEFT_FRONT_COXA = 22;
+const int LEFT_FRONT_FEMUR = 23;
+const int LEFT_FRONT_TIBIA = 24;
+const int LEFT_MIDDLE_COXA = 25;
+const int LEFT_MIDDLE_FEMUR = 26;
+const int LEFT_MIDDLE_TIBIA = 27;
+const int LEFT_BACK_COXA = 28;
+const int LEFT_BACK_FEMUR = 29;
+const int LEFT_BACK_TIBIA = 30;
+
+class Limb {
+	public:
+		Limb(int c, int f, int t, int n, std::string designator);
+		Servo coxa;
+		Servo femur;
+		Servo tibia;
+		
+		int number;
+		std::string name;
+		
+		int coxaAttachment;
+		int femurAttachment;
+		int tibiaAttachment;
+}
+
+class Organism {
+	public:
+		Organism();
+		std::vector<Limb> appendages;
+		
+		void addAppendage(Limb appendage);
+		Limb getAppendage(int index);
+}
+
+Limb::Limb(int c, int f, int t, int n, std::string designator="") : coxaAttachment(c), femurAttachment(f), tibiaAttachment(t), number(n) {
+	name = designator;
+	
+	  coxa.attach(coxaAttachment);
+	  femur.attach(coxaAttachment);
+	  tibia.attach(tibiaAttachment);
+};
+
+Organism::Organism() {
+
+}
+
+void Organism::addAppendage(Limb appendage) {
+	appendage.push_back(appendage);
+}
+
+Limb Organism::getAppendage(int index) {
+	//if (index >= 0 && index < appendages.size()) {
+	//	return appendages(index);
+	//} else {
+	//	return appendages[0];
+	//}
+	return appendages[index];
+}
 
